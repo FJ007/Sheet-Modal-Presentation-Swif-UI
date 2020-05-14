8dad6e7fc72e56e565418154552a4471d4c00ac0
Javier Fernández <kewo_007@hotmail.com>
2020-05-14 20:01:08 +0200

    Initial Commit

---
+++ PresentViewModal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PresentViewModal
+//
+//  Created by Javier Fernández on 14/05/2020.
+//  Copyright © 2020 Silversun Studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
